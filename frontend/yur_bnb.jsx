2f2c9c0c433913ecb149a2a74261f9e5c4f9b356
Joseph-Kung <joseph.kung@hotmail.com>
2018-12-11 10:50:17 -0800

    Setup static pages and entry file

---
+++ frontend/yur_bnb.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root')
+  ReactDOM.render(<h1>My Airnbnb clone</h1>, root)
+})

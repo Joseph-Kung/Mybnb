d542499223f62f8a9518c9cb8daed805235f4772
Joseph-Kung <joseph.kung@hotmail.com>
2018-12-11 13:30:57 -0800

    Add root and app components and edit entry point to take root

--- frontend/yur_bnb.jsx
+++ frontend/yur_bnb.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 
 //TEST
@@ -8,15 +9,15 @@ import { signup, login, logout } from './util/session_api_util';
 //TEST
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  const root = document.getElementById('root')
 
   //TEST
   window.signup = signup;
   window.login = login;
   window.logout = logout;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  window.store = store
   //TEST
-  const store = configureStore();
-  const root = document.getElementById('root')
-  ReactDOM.render(<h1>My Airnbnb clone</h1>, root)
+  
+  ReactDOM.render(<Root store={store} />, root)
 })

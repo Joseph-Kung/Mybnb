a92fcf29c926625e1abb1d82981dc324a3ba4fcf
Joseph-Kung <joseph.kung@hotmail.com>
2018-12-16 23:12:45 -0800

    Add bookingErrorReducer

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import sessionsReducer from './sessions/sessions_errors_reducer';
 import listingsErrorReducer from './listings/listings_errors_reducer';
+import bookingsErrorReducer from './bookings/bookings_errors_reducer';
 
 const errorsReducer = combineReducers({
   login: sessionsReducer,
   listings: listingsErrorReducer,
+  bookings: bookingsErrorReducer,
 });
 
 export default errorsReducer;
